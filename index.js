bcb5765fcba0ffbfa94ccfbe53b1d128295b18b4
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-02-01 12:29:09 +0530

    Learnt about Routing and app.get() methods

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ app.listen(port, () => {
     console.log(`app is listening on ${port}`)
 });
 
-app.use((req, res) => {
-    console.log("request received");
-    // console.log(req);
-    res.send({ "fruit": "apple", "color": "red" })
+app.get("/", (req, res) => {
+    res.send("You contacted root path");
 });
+app.get("/apple", (req, res) => {
+    res.send("You contacted apple path");
+});
+app.get("/orange", (req, res) => {
+    res.send("You contacted orange path");
+});
+
+app.post("/", (req, res) => {
+    res.send("You sent a post request to root path");
+});
+
+app.get("/", (req, res) => {
+    res.status("404").send("This path does not exist");
+});
+
+// app.use((req, res) => {
+//     console.log("request received");
+//     // console.log(req);
+//     res.send({ "fruit": "apple", "color": "red" })
+// });
